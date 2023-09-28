1d026485d1ff4ee2e75f3e4fa90bea5d09c7f675
ash358241 <ashrafunit7@gmail.com>
2023-09-29 01:25:40 +0600

    imp: login implemented and ui improved

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,22 @@
 import { Form, Input, Button } from "antd";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { loggedInUser } from "../redux/features/user/userSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+const TransparentPasswordInput = styled(Input.Password)`
+  &&& .ant-input {
+    background-color: transparent;
+  }
+`;
 interface ICredential {
   email: string;
   password: string;
 }
 
 export default function Login() {
+  const backgroundImage =
+    "https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2952&q=80";
   const dispatch = useAppDispatch();
   const { user, isLoading } = useAppSelector((state) => state.user);
   const navigate = useNavigate();
@@ -19,56 +27,102 @@ export default function Login() {
     dispatch(loggedInUser({ email, password }));
   };
 
-  if (user && !isLoading) {
+  if (user.email && !isLoading) {
     navigate("/");
   }
 
   return (
-    <div style={{ maxWidth: "400px", margin: "0 auto", padding: "20px" }}>
-      <h2>Login</h2>
-      <Form
-        name="login-form"
-        onFinish={onFinish}
-        initialValues={{ remember: true }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        position: "relative",
+      }}
+    >
+      <Link
+        to="/"
+        style={{
+          textDecoration: "none",
+          color: "#fff",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
       >
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: "email",
-              message: "Please enter a valid email!",
-            },
-            {
-              required: true,
-              message: "Please enter your email!",
-            },
-          ]}
+        <h2>BookShelf</h2>
+      </Link>
+      <div
+        style={{
+          minWidth: "400px",
+          padding: "20px",
+          background: "rgba(255, 255, 255, 0.8)",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        <h2>Login to explore</h2>
+        <Form
+          name="login-form"
+          onFinish={onFinish}
+          initialValues={{ remember: true }}
         >
-          <Input placeholder="Email" />
-        </Form.Item>
-
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your password!",
-            },
-          ]}
-        >
-          <Input.Password placeholder="Password" />
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+                message: "Please enter a valid email!",
+              },
+            ]}
           >
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+            <Input
+              placeholder="Email"
+              size="large"
+              style={{ background: "transparent" }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your password!",
+              },
+            ]}
+          >
+            <TransparentPasswordInput
+              size="large"
+              placeholder="Password"
+              style={{ background: "transparent" }}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              size="large"
+              htmlType="submit"
+              style={{
+                width: "100%",
+                borderRadius: "5px",
+                background: "rgba(0, 0, 0, 0.1)",
+                color: "#333333",
+              }}
+            >
+              <strong>Log in</strong>
+            </Button>
+          </Form.Item>
+        </Form>
+        <p style={{ textAlign: "center" }}>
+          Don't have an account? <Link to="/sign-up">Sign Up</Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,22 @@
 import { Form, Input, Button } from "antd";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { createUser } from "../redux/features/user/userSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+const TransparentPasswordInput = styled(Input.Password)`
+  &&& .ant-input {
+    background-color: transparent;
+  }
+`;
 interface ICredential {
   email: string;
   password: string;
 }
 
 export default function SignUp() {
+  const backgroundImage =
+    "https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2952&q=80";
   const dispatch = useAppDispatch();
   const { user, isLoading } = useAppSelector((state) => state.user);
   const navigate = useNavigate();
@@ -17,61 +25,102 @@ export default function SignUp() {
     dispatch(createUser({ email, password }));
   };
 
-  if (user && !isLoading) {
+  if (user.email && !isLoading) {
     navigate("/login");
   }
 
   return (
-    <div style={{ maxWidth: "400px", margin: "0 auto", padding: "20px" }}>
-      <h2 style={{ textAlign: "center" }}>Sign Up</h2>
-      <Form
-        name="signup"
-        onFinish={onFinish}
-        initialValues={{
-          remember: true,
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        position: "relative",
+      }}
+    >
+      <Link
+        to="/"
+        style={{
+          textDecoration: "none",
+          color: "#fff",
+          position: "absolute",
+          top: 0,
+          left: 0,
         }}
       >
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your email!",
-              type: "email",
-            },
-          ]}
+        <h2>BookShelf</h2>
+      </Link>
+      <div
+        style={{
+          minWidth: "400px",
+          padding: "20px",
+          background: "rgba(255, 255, 255, 0.8)",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        <h2>Create an account</h2>
+        <Form
+          name="sign-up-form"
+          onFinish={onFinish}
+          initialValues={{
+            remember: true,
+          }}
         >
-          <Input
-            placeholder="Email"
-            size="large"
-            style={{ borderRadius: "5px" }}
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your password!",
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder="Password"
-            size="large"
-            style={{ borderRadius: "5px" }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            style={{ width: "100%", borderRadius: "5px" }}
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email!",
+                type: "email",
+              },
+            ]}
           >
-            Sign Up
-          </Button>
-        </Form.Item>
-      </Form>
+            <Input
+              placeholder="Email"
+              size="large"
+              style={{ background: "transparent" }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your password!",
+              },
+            ]}
+          >
+            <TransparentPasswordInput
+              placeholder="Password"
+              style={{ background: "transparent" }}
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              size="large"
+              htmlType="submit"
+              style={{
+                width: "100%",
+                borderRadius: "5px",
+                background: "rgba(0, 0, 0, 0.1)",
+                color: "#333333",
+              }}
+            >
+              <strong>Sign Up</strong>
+            </Button>
+          </Form.Item>
+        </Form>
+        <p style={{ textAlign: "center" }}>
+          Already have an account? <Link to="/login">Log in</Link>
+        </p>
+      </div>
     </div>
   );
 }
