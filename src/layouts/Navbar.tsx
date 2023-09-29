1e6e91270b43d29dba6c74621c95ac390b1c1540
ash358241 <ashrafunit7@gmail.com>
2023-09-29 07:23:53 +0600

    init: navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Layout, Menu } from "antd";
+import {
+  HomeOutlined,
+  BookOutlined,
+  ShoppingCartOutlined,
+  LoginOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return <div>Navbar</div>;
-}
+const { Header } = Layout;
+
+const AppNavbar: React.FC = () => {
+  return (
+    <Header>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<BookOutlined />}>
+          <Link to="all-books">All Books</Link>
+        </Menu.Item>
+        <Menu.Item key="3" icon={<ShoppingCartOutlined />}>
+          <Link to="/checkout">Checkout</Link>
+        </Menu.Item>
+        <Menu.Item key="4" icon={<LoginOutlined />}>
+          <Link to="/login">Login</Link>
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+};
+
+export default AppNavbar;
