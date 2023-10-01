19ba77af78c19f4fd7d9f6daae21554db8944578
ash358241 <ashrafunit7@gmail.com>
2023-10-01 15:54:58 +0600

    imp: formed the add-book form

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Form, Input, Button, DatePicker, Row, Col } from "antd";
+import { Form, Input, Button, DatePicker, Row, Col, Upload } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import "../styles/AddBook.css";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
 const AddBook = () => {
   const [form] = Form.useForm();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
 
   const onFinish = (values: unknown) => {
     setIsSubmitting(true);
@@ -14,6 +16,25 @@ const AddBook = () => {
       setIsSubmitting(false);
       form.resetFields();
     }, 1000);
+  };
+
+  const onChange: UploadProps["onChange"] = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const onPreview = async (file: UploadFile) => {
+    let src = file.url as string;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj as RcFile);
+        reader.onload = () => resolve(reader.result as string);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow?.document.write(image.outerHTML);
   };
 
   return (
@@ -28,6 +49,7 @@ const AddBook = () => {
           author: "",
           genre: "",
           publicationDate: null,
+          image: null,
         }}
       >
         <Row gutter={16}>
@@ -42,7 +64,7 @@ const AddBook = () => {
                 },
               ]}
             >
-              <Input />
+              <Input size="large" />
             </Form.Item>
           </Col>
           <Col span={12}>
@@ -56,7 +78,7 @@ const AddBook = () => {
                 },
               ]}
             >
-              <Input />
+              <Input size="large" />
             </Form.Item>
           </Col>
         </Row>
@@ -72,7 +94,7 @@ const AddBook = () => {
                 },
               ]}
             >
-              <Input />
+              <Input size="large" />
             </Form.Item>
           </Col>
           <Col span={12}>
@@ -87,13 +109,39 @@ const AddBook = () => {
                 },
               ]}
             >
-              <DatePicker style={{ width: "100%" }} />
+              <DatePicker style={{ width: "100%" }} size="large" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Image"
+              name="image"
+              rules={[
+                {
+                  required: true,
+                  type: "object",
+                  message: "Please enter the book's image",
+                },
+              ]}
+            >
+              <Upload
+                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                listType="picture-card"
+                fileList={fileList}
+                onChange={onChange}
+                onPreview={onPreview}
+              >
+                {fileList.length < 5 && "+ Upload"}
+              </Upload>
             </Form.Item>
           </Col>
         </Row>
         <Form.Item>
           <Button
             type="primary"
+            size="large"
             htmlType="submit"
             icon={<PlusOutlined />}
             loading={isSubmitting}
